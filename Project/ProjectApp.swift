e06ec9d689b1d51d7b2247761967b2016b3e9d23
Nohelia Da Silva <nohelia@lawn-10-91-47-232.lawn.gatech.edu>
2024-02-09 15:18:52 -0500

    first commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Nohelia Da Silva on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
